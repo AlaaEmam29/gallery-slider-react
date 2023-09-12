bd6b592de82e19be53a9f0965811b7bb3e976805
Alaa Shaban <devalaashaban29@gmail.com>
2023-09-12 11:31:31 +0300

    Fix: Production Issues

--- src/context/contextImages.jsx
+++ src/context/contextImages.jsx
@@ -73,7 +73,6 @@ const reducer = (state, action) => {
   }
 };
 
-const URL = "http://localhost:8080";
 const contextImages = createContext();
 export const ImagesProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -81,7 +80,7 @@ export const ImagesProvider = ({ children }) => {
     dispatch({ type: imagesType.LOADING, payload: true });
 
     try {
-      const response = await axios.get(`images.json`);
+      const response = await axios.get(`/images.json`);
       dispatch({ type: imagesType.SUCCESS, payload: response.data });
     } catch (error) {
       dispatch({ type: imagesType.ERROR, payload: error.message });
